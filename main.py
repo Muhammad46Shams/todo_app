5c67b660c1fcf56dd44b538e18fc64014d801d9b
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-09-01 19:43:29 +0100

    convert string to no and how to access items of list and replace the items with new value

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 todos = []
 while True:
-    user_action = input('Type add, show or exit: ')
+    user_action = input('Type add, show, edit or exit: ')
     user_action = user_action.strip()
     
     match user_action:
@@ -12,6 +12,10 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input('No of the todo for edit: '))
+            number = number -1
+            todos[number] = input('enter new todo: ').strip()
         case 'exit':
             break
        
