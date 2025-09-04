f0c69e78b1c124d80a684e024fc508b92614bc19
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-09-04 13:50:03 +0100

    use pop method to remove items from the list

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 todos = []
 while True:
-    user_action = input('Type add, show, edit or exit: ')
+    user_action = input('Type add, show, edit, complete or exit: ')
     user_action = user_action.strip()
     
     match user_action:
@@ -11,12 +11,15 @@ while True:
         case 'show':
             for index, item in enumerate(todos):
                 item = item.title()
-                row = f"{index}-{item}"
+                row = f"{index + 1}-{item}"
                 print(row)
         case 'edit':
             number = int(input('No of the todo for edit: '))
             number = number -1
             todos[number] = input('enter new todo: ').strip()
+        case 'complete':
+            number = int(input('No of the todo for complete: '))
+            todos.pop(number -1)
         case 'exit':
             break
        
