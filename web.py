d45de91ad995e27690927b4a92e91ead24e6024f
Muhammad46Shams <shams,muhammad46@gmail.com>
2025-10-16 23:10:57 +0100

    Implement complete todo funcationality

--- web.py
+++ web.py
@@ -13,9 +13,12 @@ st.title('My todo App')
 st.subheader('This is todo app')
 st.write('this app is to increase productivity')
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
     
 st.text_input(label="New todo", placeholder='Add todo', on_change=add_todo, key='new_todo')
-
-st.session_state
